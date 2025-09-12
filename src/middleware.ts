11a7481223c2aa8f1f2be16412b88de5ac66a47c
Jzjha <jzjha215@gmail.com>
2025-09-12 17:31:59 +0000

    Error: Cannot find module 'node:process': Unsupported external type Url

--- src/middleware.ts
+++ src/middleware.ts
@@ -43,4 +43,5 @@ export async function middleware(request: NextRequest) {
 // See "Matching Paths" below to learn more
 export const config = {
   matcher: ['/admin/:path*', '/login'],
+  runtime: 'nodejs',
 };
