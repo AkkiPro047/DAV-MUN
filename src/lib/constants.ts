a96bc56051b5fae65a11b06ff53aa029be95b580
Jzjha <jzjha215@gmail.com>
2025-09-13 11:54:21 +0000

    change the admin V6jUsyelyWNmUv1Cs6oFFavhFiy2

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 // This is the unique User ID (UID) from Firebase Authentication for the user who will have admin privileges.
 // To change the admin user, replace the UID below with the new admin's UID from the Firebase Console.
-export const ADMIN_UID = 'Rhj3R3dN11RynpT5gP5fF445pRH3';
+export const ADMIN_UID = 'V6jUsyelyWNmUv1Cs6oFFavhFiy2';
